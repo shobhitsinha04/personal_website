486264451e89b94789deedf11bc73daa384f065e
shobhitsinha04 <shobhitsinha04@gmail.com>
2024-10-10 19:46:20 -0500

    changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,44 +3,21 @@ import { projects } from '../../portfolio'
 import ProjectContainer from '../ProjectContainer/ProjectContainer'
 import './Projects.css'
 
-// const Projects = () => {
-//   if (!projects.length) return null
-
-//   return (
-//     <section id='projects' className='section projects'>
-//       <h2 className='section__title'>Projects</h2>
-
-//       <div className='projects__grid'>
-//         {projects.map((project) => (
-//           <ProjectContainer key={uniqid()} project={project} />
-//         ))}
-//       </div>
-//     </section>
-//   )
-// }
-
 const Projects = () => {
-  if (!projects.length) return null;
+  if (!projects.length) return null
 
   return (
     <section id='projects' className='section projects'>
-      <h2 className='section__title'>Experience</h2>
-      <div className='projects_list'>
-        {experience.map((exp) => (
-          <div key={uniqid()} className='projects__item'>
-            <h3 className='projects__role'>{exp.name}</h3>
-            <ul className='projects__description'>
-              {exp.description.map((desc, idx) => (
-                <li key={idx}>{desc}</li>
-              ))}
-            </ul>
-          </div>
+      <h2 className='section__title'>Projects</h2>
+
+      <div className='projects__grid'>
+        {projects.map((project) => (
+          <ProjectContainer key={uniqid()} project={project} />
         ))}
       </div>
     </section>
-  );
-};
-
+  )
+}
 
 
 export default Projects
